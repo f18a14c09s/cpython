78df0fc185ab356d6b4ae250f3d19a7f9e24bc4b
Francis Johnson <business@francisjohnson.org>
2022-05-07 00:32:44 -0400

    Converted from PyTest to vanilla Unittest.

--- Lib/test/test_email/issue_44660/default_email_policy/test_contentmanager.py
+++ Lib/test/test_email/issue_44660/default_email_policy/test_contentmanager.py
@@ -1,6 +1,6 @@
 from email.message import EmailMessage
 
-import pytest
+from unittest import TestCase
 
 
 def utf8_header_field_params():
@@ -20,44 +20,45 @@ def utf8_header_field_params():
     ]
 
 
-@pytest.mark.parametrize(
-    'field_name,field_value_format_str',
-    utf8_header_field_params()
-)
-def test_rfc6532_set_global_message_content(field_name, field_value_format_str):
-    """
-    Explicit configuration of "global" message subtype and "quoted-printable"
-     Content-Transfer-Encoding when message with UTF-8 in header field value
-     is set as the content of (i.e. nested in) a top-level message.
-    """
+class TestContentManager(TestCase):
+    def test_rfc6532_set_global_message_content(self):
+        """
+        Explicit configuration of "global" message subtype and "quoted-printable"
+         Content-Transfer-Encoding when message with UTF-8 in header field value
+         is set as the content of (i.e. nested in) a top-level message.
+        """
 
-    message = EmailMessage()
-    assert isinstance(message.get_content_type(), str)
-    orig_content_type = str(message.get_content_type())
-    assert orig_content_type
-    global_message = EmailMessage()
-    utf8_str_1 = 'ⒶⒷⒸ'
-    utf8_str_2 = '①②③'
-    expected_value = field_value_format_str.format(utf8_str_1, utf8_str_2)
-    global_message[field_name] = expected_value
-    expected_cte = 'quoted-printable'
+        for field_name, field_value_format_str in utf8_header_field_params():
+            with self.subTest(field_name):
+                message = EmailMessage()
+                self.assertIsInstance(message.get_content_type(), str)
+                orig_content_type = str(message.get_content_type())
+                self.assertTrue(orig_content_type)
+                global_message = EmailMessage()
+                utf8_str_1 = 'ⒶⒷⒸ'
+                utf8_str_2 = '①②③'
+                expected_value = field_value_format_str.format(utf8_str_1,
+                                                               utf8_str_2)
+                global_message[field_name] = expected_value
+                expected_cte = 'quoted-printable'
 
-    message.set_content(
-        global_message,
-        subtype='global',
-        cte=expected_cte
-    )
+                message.set_content(
+                    global_message,
+                    subtype='global',
+                    cte=expected_cte
+                )
 
-    assert isinstance(message.get_content_type(), str)
-    assert message.get_content_type()
-    assert message.get_content_type() != orig_content_type
-    # Make sure Content-Type is not set to "message/rfc822," as none of the RFCs
-    # prior to 6532 support UTF-8:
-    assert message.get_content_type() == 'message/global', (
-        'Wrong Content-Type set as described at top of RFC 6532, s. 3.7.'
-    )
-    assert message.get_all(
-        'Content-Transfer-Encoding'
-    ) == [
-               expected_cte
-           ], 'Content-Transfer-Encoding not set to specified value.'
+                self.assertIsInstance(message.get_content_type(), str)
+                self.assertTrue(message.get_content_type())
+                self.assertNotEqual(message.get_content_type(),
+                                    orig_content_type)
+                # Make sure Content-Type is not set to "message/rfc822," as none of the RFCs
+                # prior to 6532 support UTF-8:
+                self.assertEqual(message.get_content_type(), 'message/global', (
+                    'Wrong Content-Type set as described at top of RFC 6532, s. 3.7.'
+                ))
+                self.assertEqual(
+                    message.get_all('Content-Transfer-Encoding'),
+                    [expected_cte],
+                    'Content-Transfer-Encoding not set to specified value.',
+                )
--- Lib/test/test_email/issue_44660/default_email_policy/test_generators.py
+++ Lib/test/test_email/issue_44660/default_email_policy/test_generators.py
@@ -2,7 +2,7 @@ import email
 import email.policy
 from email.message import EmailMessage
 
-import pytest
+from unittest import TestCase
 
 
 def utf8_header_field_params():
@@ -22,73 +22,80 @@ def utf8_header_field_params():
     ]
 
 
-@pytest.mark.parametrize(
-    'field_name,field_value_format_str',
-    utf8_header_field_params()
-)
-def test_rfc6532_set_global_message_content(field_name, field_value_format_str):
-    """
-    Generation of RFC 822 string that has ASCII characters only, from message
-     object whose content:
-     - Is another message object with UTF-8 in header field value; and
-     - Has explicit configuration of "global" message subtype and
-      "quoted-printable" Content-Transfer-Encoding.
-    """
+class TestGenerators(TestCase):
+    def test_rfc6532_set_global_message_content(self):
+        """
+        Generation of RFC 822 string that has ASCII characters only, from message
+         object whose content:
+         - Is another message object with UTF-8 in header field value; and
+         - Has explicit configuration of "global" message subtype and
+          "quoted-printable" Content-Transfer-Encoding.
+        """
 
-    message = EmailMessage()
-    assert isinstance(message.get_content_type(), str)
-    orig_content_type = str(message.get_content_type())
-    assert orig_content_type
-    global_message = EmailMessage()
-    utf8_str_1 = 'ⒶⒷⒸ'
-    utf8_str_2 = '①②③'
-    field_value = field_value_format_str.format(utf8_str_1, utf8_str_2)
-    expected_encoded_field_value = (
-        field_value_format_str.format(
-            '=E2=92=B6=E2=92=B7=E2=92=B8',
-            '=E2=91=A0=E2=91=A1=E2=91=A2'
-        )
-    )
-    global_message[field_name] = field_value
-    expected_cte = 'quoted-printable'
-    message.set_content(
-        global_message,
-        subtype='global',
-        cte=expected_cte
-    )
+        for field_name, field_value_format_str in utf8_header_field_params():
+            with self.subTest(field_name):
+                message = EmailMessage()
+                self.assertIsInstance(
+                    message.get_content_type(), str
+                )
+                orig_content_type = str(message.get_content_type())
+                self.assertTrue(orig_content_type)
+                global_message = EmailMessage()
+                utf8_str_1 = 'ⒶⒷⒸ'
+                utf8_str_2 = '①②③'
+                field_value = field_value_format_str.format(utf8_str_1,
+                                                            utf8_str_2)
+                expected_encoded_field_value = (
+                    field_value_format_str.format(
+                        '=E2=92=B6=E2=92=B7=E2=92=B8',
+                        '=E2=91=A0=E2=91=A1=E2=91=A2'
+                    )
+                )
+                global_message[field_name] = field_value
+                expected_cte = 'quoted-printable'
+                message.set_content(
+                    global_message,
+                    subtype='global',
+                    cte=expected_cte
+                )
 
-    actual_message_str = message.as_string(
-        policy=email.policy.default.clone(
-            utf8=False, cte_type='7bit', max_line_length=200, linesep='\r\n'
-        )
-    )
-    assert actual_message_str == (
-        'Content-Type: message/global\r\n'
-        'Content-Transfer-Encoding: quoted-printable\r\n'
-        'MIME-Version: 1.0\r\n'
-        '\r\n'
-        f'{field_name}: {expected_encoded_field_value}\r\n'
-        '\r\n'
-    )
+                actual_message_str = message.as_string(
+                    policy=email.policy.default.clone(
+                        utf8=False, cte_type='7bit', max_line_length=200,
+                        linesep='\r\n'
+                    )
+                )
+                self.assertEqual(
+                    actual_message_str, (
+                        'Content-Type: message/global\r\n'
+                        'Content-Transfer-Encoding: quoted-printable\r\n'
+                        'MIME-Version: 1.0\r\n'
+                        '\r\n'
+                        f'{field_name}: {expected_encoded_field_value}\r\n'
+                        '\r\n'
+                    )
+                )
 
+    def test_rfc6532_global_message_generation(self):
+        """
+        Generation of RFC 822 string that has UTF-8 characters, from message object
+         that has header field value with UTF-8 characters.
+        """
 
-@pytest.mark.parametrize(
-    'field_name,field_value_format_str',
-    utf8_header_field_params()
-)
-def test_rfc6532_global_message_generation(field_name, field_value_format_str):
-    """
-    Generation of RFC 822 string that has UTF-8 characters, from message object
-     that has header field value with UTF-8 characters.
-    """
-
-    global_message = EmailMessage()
-    utf8_str_1 = 'ⒶⒷⒸ'
-    utf8_str_2 = '①②③'
-    expected_value = field_value_format_str.format(utf8_str_1, utf8_str_2)
-    global_message[field_name] = expected_value
-    assert global_message.as_string(
-        policy=email.policy.default.clone(
-            utf8=True, cte_type='8bit', max_line_length=200, linesep='\r\n'
-        )
-    ) == f'{field_name}: {expected_value}\r\n\r\n'
+        for field_name, field_value_format_str in utf8_header_field_params():
+            with self.subTest(field_name):
+                global_message = EmailMessage()
+                utf8_str_1 = 'ⒶⒷⒸ'
+                utf8_str_2 = '①②③'
+                expected_value = field_value_format_str.format(utf8_str_1,
+                                                               utf8_str_2)
+                global_message[field_name] = expected_value
+                self.assertEqual(
+                    global_message.as_string(
+                        policy=email.policy.default.clone(
+                            utf8=True, cte_type='8bit', max_line_length=200,
+                            linesep='\r\n'
+                        )
+                    ),
+                    f'{field_name}: {expected_value}\r\n\r\n',
+                )
